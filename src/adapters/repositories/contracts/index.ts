ed60f40097e3295aee12e2659d4fb97e2424f1e4
Claudio Condor <claucondor@gmail.com>
2024-02-09 23:01:17 -0400

    Add parseItemsToPurchaseData function to ContractsRepository

--- src/adapters/repositories/contracts/index.ts
+++ src/adapters/repositories/contracts/index.ts
@@ -1,6 +1,10 @@
 import { MondayClient } from '../../../infraestructure/monday/client';
 import { IContractsRepository } from './interface';
-import { parseItemsToClienteData, parseItemsToPropertyData } from './utils';
+import {
+  parseItemsToClienteData,
+  parseItemsToPropertyData,
+  parseItemsToPurchaseData,
+} from './utils';
 
 export class ContractsRepository implements IContractsRepository {
   private mondayClient: MondayClient;
@@ -11,17 +15,16 @@ export class ContractsRepository implements IContractsRepository {
 
   async getBoardInfo(): Promise<any> {
     const board1 = await this.mondayClient.getBoardData(5945847430);
-    console.log(board1.data.boards[0].items);
     const propertyDataArray = parseItemsToPropertyData(board1.data.boards[0].items);
     console.log(propertyDataArray);
 
     const board2 = await this.mondayClient.getBoardData(5945933605);
-    console.log(board2.data.boards[0].items);
     const clienteDataArray = parseItemsToClienteData(board2.data.boards[0].items);
     console.log(clienteDataArray);
 
     const board3 = await this.mondayClient.getBoardData(5945851807);
-    console.log(board3.data.boards[0].items);
+    const purchaseDataArray = parseItemsToPurchaseData(board3.data.boards[0].items);
+    console.log(purchaseDataArray);
     return;
   }
 }
